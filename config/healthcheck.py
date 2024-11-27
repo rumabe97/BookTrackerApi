0f184f6f01c3171d1553e40eb8c268a34d1c7443
rumabe97 <rumabe97@gmail.com>
2024-11-27 17:16:41 +0100

    Check last version

--- config/healthcheck.py
+++ config/healthcheck.py
@@ -2,11 +2,14 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.http import JsonResponse
 import requests
-
+from packaging import version
+from shared.GoogleDriveVersions import get_latest_version
 from shared.googleBookApi import healthcheckApi
 
 
 def healthcheck(request):
+    userVersion = request.GET.get('version', None)
+
     health_status = {
         "database": "ok",
         "google_books_api": "ok",
@@ -29,4 +32,23 @@ def healthcheck(request):
         health_status["google_books_api"] = "error"
         health_status["status"] = "unhealthy"
 
+    if userVersion:
+        latest_version, latest_file_id = get_latest_version()
+
+        if version.parse(userVersion) < version.parse(latest_version):
+            download_url = f'https://drive.google.com/uc?id={latest_file_id}'
+            health_status.update({
+                'outdated': True,
+                'latest_version': latest_version,
+                'download_url': download_url
+            })
+        else:
+            health_status.update({
+                'outdated': False,
+                'latest_version': latest_version
+            })
+    else:
+        health_status['outdated'] = None
+        health_status['latest_version'] = 'N/A'
+        health_status['download_url'] = None
     return JsonResponse(health_status, status=200 if health_status["status"] == "healthy" else 500)
---
+++ shared/GoogleDriveVersions.py
@@ -0,0 +1,37 @@
+import re
+import json
+from email.policy import default
+
+from packaging import version
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+from decouple import config
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+google_credentials_json = config('GOOGLE_CREDENTIALS_JSON')
+
+credentials_dict = json.loads(google_credentials_json)
+credentials = Credentials.from_service_account_info(credentials_dict)
+
+drive_service = build('drive', 'v3', credentials=credentials)
+folderId = config('FOLDER_ID', default='188n6CCpA_TbuNJ0aztENdCGFr2XHZVfK')
+
+
+def get_latest_version():
+    query = f"'{folderId}' in parents and trashed = false"
+    results = drive_service.files().list(q=query, fields="files(id, name)").execute()
+    files = results.get('files', [])
+
+    latest_version = '0.0.0'
+    latest_file_id = None
+
+    for file in files:
+        name = file.get('name', '')
+        match = re.match(r'BookTracker-(\d+\.\d+\.\d+)\.apk$', name)
+        if match:
+            file_version = match.group(1)
+            if version.parse(file_version) > version.parse(latest_version):
+                latest_version = file_version
+                latest_file_id = file.get('id')
+
+    return latest_version, latest_file_id
